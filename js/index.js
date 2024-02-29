2610ca84f9b0fca8d60de934a0cac4086449a434
김세양 <tpdid2645@naver.com>
2024-02-29 16:28:50 +0900

    index update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+  /* main-slide */
+  let mainSwiper = new Swiper(".main-swiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
   /* youth-supply-swiper */
   const supplySwiper = new Swiper(".youth-supply-swiper", {
     slidesPerView: 4.3,
     spaceBetween: 24,
+    speed:1200,
+    autoplay: {
+      delay: 1800,
+    },
     loop: true,
   });
   /* hot-swiper */
@@ -21,6 +39,11 @@ document.addEventListener("DOMContentLoaded", function () {
     autoplay: {
       delay: 1800,
     },
-    speed:1500,
+    speed: 1500,
+  });
+
+  /* aos js */
+  AOS.init({
+    duration: 1200,
   });
 }); /* document ready 끝 */
